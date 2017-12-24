a16414b6b714424ff6548c4bd1c3459dbe1422cf
Reginald Tan <redge.tan@gmail.com>
2017-12-24 09:33:33 -0500

    access to history

--- client/application.js
+++ client/application.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './router/index';
-import { BrowserRouter } from 'react-router-dom'
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+
+import { GC_USER_ID, GC_AUTH_TOKEN } from './config/config'
+
+const currentUserId = localStorage.getItem(GC_USER_ID)
+
 const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjbjm5ifj0cf60172osy2g4lp' })
 
-// 3
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
 })
 
+window.browserHistory = createBrowserHistory()
+
 ReactDOM.render(
-  <BrowserRouter>
+  <Router history={browserHistory}>
     <ApolloProvider client={client}>
       <AppRouter />
     </ApolloProvider>
-  </BrowserRouter>, 
+  </Router>, 
 document.getElementById('root'))
+
+
+
+/* integrate regular html with react */
+document.querySelector(".home_logout_btn").addEventListener("click", (event) => {
+   localStorage.removeItem(GC_USER_ID)
+   localStorage.removeItem(GC_AUTH_TOKEN)
+   browserHistory.push("/")
+})
+
+if (currentUserId) {
+  document.querySelector(".login_btn").style.display = 'none'
+  document.querySelector(".signup_btn").style.display = 'none'
+  document.querySelector(".home_user_menu").style.display = 'block'
+}
--- client/router/index.js
+++ client/router/index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 
 import {
   Route,
-  Link
+  Link,
+  Switch
 } from 'react-router-dom'
 
 import Home from './../components/Home'
 import About from './../components/About'
 import Signup from './../components/Signup'
+import Login from './../components/Login'
 import UserScreen from './../screens/UserScreen'
 
 
@@ -15,10 +17,13 @@ export default class AppRouter extends Component {
 	render() {
 		return (
 			<div>
-	      <Route exact path="/messages" component={Home}/>
-	      <Route exact path="/about" component={About}/>
-	      <Route exact path="/signup" component={Signup}/>
-	      <Route exact path="/" component={UserScreen}/>
+				<Switch>
+		      <Route exact path="/messages" component={Home}/>
+		      <Route exact path="/about" component={About}/>
+	        <Route exact path='/login' component={Login}/>
+		      <Route exact path="/signup" component={Signup}/>
+		      <Route exact path="/" component={UserScreen}/>
+				</Switch>
 			</div>
 		)
 	}
