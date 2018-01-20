e5405ae9cd7e586c2450fd0b482ccee06a67fa5b
Reginald Tan <redge.tan@gmail.com>
2018-01-19 21:36:35 -0500

    a

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ProfileCard from './../components/ProfileCard'
 import ClientAPI from './../api/client_api'
+const Rating = require('react-rating')
 
 
 export default class UserDirectoryScreen extends Component {
@@ -38,15 +39,66 @@ export default class UserDirectoryScreen extends Component {
             Improve your chance of success in Tinder, OkCupid, Match.com, Bumble, Coffee Meets Bagel
           </div>
           <div className="home_sample_photo_container">
-            <img className="home_sample_photo" src="/assets/afro.jpg" alt=""/>
-            <img className="home_sample_photo" src="/assets/front_bike.png" alt=""/>
-            <img className="home_sample_photo" src="/assets/kimono.jpg" alt=""/>
+            <div className="photo_card">
+              <div className="author_header">
+                <div className="user_avatar"></div>
+                <div className="username"></div>
+              </div>
+              <img className="home_sample_photo" src="/assets/afro.jpg" alt=""/>
+              <div className="user_rating">
+                <Rating 
+                  emptySymbol="fa fa-star-o"
+                  fullSymbol="fa fa-star"
+                  readonly={true}
+                  initialRating={5}
+                />
+
+              </div>
+              <div className="cost"><span class="session_price">$60</span> per person</div>
+            </div>
+
+            <div className="photo_card">
+              <div className="author_header">
+                <div className="user_avatar"></div>
+                <div className="username"></div>
+              </div>
+              <img className="home_sample_photo" src="/assets/front_bike.png" alt=""/>
+              <div className="user_rating">
+                <Rating 
+                  emptySymbol="fa fa-star-o"
+                  fullSymbol="fa fa-star"
+                  readonly={true}
+                  initialRating={5}
+                />
+
+              </div>
+              <div className="cost"><span class="session_price">$95</span> per person</div>
+            </div>
+
+            <div className="photo_card">
+              <div className="author_header">
+                <div className="user_avatar"></div>
+                <div className="username"></div>
+              </div>
+              <img className="home_sample_photo" src="/assets/kimono.jpg" alt=""/>
+              <div className="user_rating">
+                <Rating 
+                  emptySymbol="fa fa-star-o"
+                  fullSymbol="fa fa-star"
+                  readonly={true}
+                  initialRating={5}
+                />
+
+              </div>
+              <div className="cost"><span class="session_price">$130</span> per person</div>
+            </div>
+
           </div>
         </div>
 
         <div className="container how_it_works_container">
-          <h2>How it Works</h2>
-          <ul>
+          <h1>How it Works</h1>
+          <ul className="how_it_works_list light_gray">
             <li>Choose a Photographer</li>
             <li>Pick a Date/Time</li>
             <li>Meetup up with photographer and capture beautiful moments</li>
