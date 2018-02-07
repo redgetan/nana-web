0b1588d223006248120bac81b8aa8f43cd97b35d
Reginald Tan <redge.tan@gmail.com>
2018-02-07 08:18:27 -0500

    lazy load for faster loading time

--- client/components/PhotoSlider.js
+++ client/components/PhotoSlider.js
@@ -36,6 +36,7 @@ export default class PhotoSlider extends Component {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    lazyLoad: true,
     prevArrow: <PrevArrow />,
     nextArrow: <NextArrow />
   }
