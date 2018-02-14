529e6eeb481d0700d36d72b25ac28688728389b7
Reginald Tan <redge.tan@gmail.com>
2018-02-14 00:06:58 -0500

    update homescreen

--- client/components/Photographer/PhotographerDirectory.js
+++ client/components/Photographer/PhotographerDirectory.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import classNames from 'classnames'
 import ProfileCard from './ProfileCard'
 
+import { Link } from 'react-router-dom'
+
 export default class PhotographerDirectory extends Component {
   render() {
     if (this.props.users.length === 0) {
@@ -17,10 +19,17 @@ export default class PhotographerDirectory extends Component {
     } 
 
     return (
-      <div className="photographer_directory container-fluid">
+      <div className="photographer_directory container">
+        <h1>Toronto Photographers</h1>
+        <br/>
         {
           this.props.users.map((user) => (
-            <ProfileCard key={user.id} user={user}/>
+            <div className='user_avatar_container col-xs-6 col-sm-3 col-md-2' key={user.id} >
+              <Link to={`/users/${user.id}`} >
+                <img className='user_avatar' src={user.avatar} alt=""/>
+                <div className="username">{user.username}</div>
+              </Link>
+            </div>
           ))
         }
       </div>
