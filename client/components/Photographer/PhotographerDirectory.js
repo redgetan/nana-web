303e56e28ac0ea2605b51a3c6da57fd3f86d94e5
Reginald Tan <redge.tan@gmail.com>
2018-02-26 11:27:14 -0500

    photographers

--- client/components/Photographer/PhotographerDirectory.js
+++ client/components/Photographer/PhotographerDirectory.js
@@ -4,6 +4,17 @@ import ProfileCard from './ProfileCard'
 
 import { Link } from 'react-router-dom'
 
+const groupBy = (xs, key) => {
+  return xs.reduce((rv, x) => {
+    (rv[x[key]] = rv[x[key]] || []).push(x)
+    return rv
+  }, {})
+}
+
+const capitalizeWords = (text) => {
+  return text.replace(/\b\w/g, l => l.toUpperCase())
+}
+
 export default class PhotographerDirectory extends Component {
   render() {
     if (this.props.users.length === 0) {
@@ -18,19 +29,33 @@ export default class PhotographerDirectory extends Component {
       )
     } 
 
+    let contents = []
+    let usersByLocation = groupBy(this.props.users, "location")
+
+    for (let location in usersByLocation) {
+      contents.push(
+        <div>
+          <h1>{capitalizeWords(location)} Photographers</h1>
+          <br/>
+        </div>
+      )
+
+      usersByLocation[location].map((user) => {
+        contents.push(
+          <div className='user_avatar_container col-xs-6 col-sm-3 col-md-2' key={user.id} >
+            <Link to={`/users/${user.id}`} >
+              <img className='user_avatar' src={user.avatar} alt=""/>
+              <div className="username">{user.username}</div>
+            </Link>
+          </div>
+        )
+      })
+    }
+
     return (
       <div className="photographer_directory container">
-        <h1>Toronto Photographers</h1>
-        <br/>
         {
-          this.props.users.map((user) => (
-            <div className='user_avatar_container col-xs-6 col-sm-3 col-md-2' key={user.id} >
-              <Link to={`/users/${user.id}`} >
-                <img className='user_avatar' src={user.avatar} alt=""/>
-                <div className="username">{user.username}</div>
-              </Link>
-            </div>
-          ))
+          contents
         }
       </div>
     )
