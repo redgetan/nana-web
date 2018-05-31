c749b0e14a9db48f72ae31a00a094c7fb31ba668
Reginald Tan <redge.tan@gmail.com>
2018-05-31 20:44:03 +0300

    book request when/where/duration

--- client/components/Photographer/BookRequest.js
+++ client/components/Photographer/BookRequest.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
 const Rating = require('react-rating')
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+
 import Config from './../../config/config'
 import ClientAPI from './../../api/client_api'
 import ContactForm from "./ContactForm"
+import Select from 'react-select';
+import SelectField from "./../Widget/SelectField"
 
 export default class BookRequest extends Component {
+
+  state = {
+    startDate: null,
+    endDate: null,
+  }
+
+  handleStartChange = (date) => {
+    this.setState({
+      startDate: date
+    })
+  }
+
+  handleEndChange = (date) => {
+    this.setState({
+      endDate: date
+    })
+  }
 
   componentWillMount() {
   }
@@ -12,12 +34,56 @@ export default class BookRequest extends Component {
   componentWillUnmount() {
   }
 
+  handleDurationChange() {
+
+  }
+
   render() {
     const price = this.props.user.price ? this.props.user.price : "90-200"
+    const minEndDate = this.state.startDate ? this.state.startDate : moment()
+    const selectedOption = 'two'
+
     return (
       <div className="book_request_form col-xs-12 col-sm-5">
         <form className='' onSubmit={this.bookRequest}>
           <div className="cost"><span className="session_price">$ {price}</span> per hour</div>
+          <div className="photoshoot_date_range">
+            <div className='photoshoot_details_row'>
+              <label>When</label>
+              <div className='photoshoot_block'>
+                <DatePicker
+                    dateFormat="LL"
+                    placeholderText="Start Time"
+                    selected={this.state.startDate}
+                    onChange={this.handleStartChange}
+                    minDate={moment()}
+                />
+              </div>
+            </div>
+            <div className='photoshoot_details_row'>
+              <label>Where</label>
+              <div>
+                <input type="text" placeholder='exact address to meet' />
+              </div>
+            </div>
+            <div className='photoshoot_details_row'>
+              <label>Duration</label>
+              <div>
+              <Select
+                name="duration"
+                clearable={false}
+                searchable={false}
+                value={selectedOption}
+                onChange={this.handleDurationChange}
+                options={[
+                  { value: 'one', label: '1 hour - 30 photos' },
+                  { value: 'two', label: '2 hours - 60 photos' },
+                  { value: 'three', label: '3 hours - 90 photos' },
+                ]}
+              />
+              </div>
+            </div>
+          </div>
           <div className='user_rating'>
             <Rating 
               emptySymbol="fa fa-star-o fa"
