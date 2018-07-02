455588865c074ca94c83a60950080cab1c4eec0c
Reginald Tan <redge.tan@gmail.com>
2018-07-02 14:30:08 -0400

    update instagram id

--- client/config/config.js
+++ client/config/config.js
@@ -56,13 +56,13 @@ export default class Config {
   static c() {
     if (process.env.NODE_ENV === "production") {
       return {
-        INSTAGRAM_CLIENT_ID: "e1a7ed3d7af44eea8ccad4a5fced1bf5",
+        INSTAGRAM_CLIENT_ID: "972db678348f44a3b511b5dcfe096acc",
         INSTAGRAM_REDIRECT_URI: "https://5jbouftijk.execute-api.us-west-2.amazonaws.com/production/login/callback",
         STRIPE_PUBLIC_KEY: "pk_live_0SvoLzprLiFPyrhb1XUDCWRK"
       }
     } else {
       return {
-        INSTAGRAM_CLIENT_ID: "e1a7ed3d7af44eea8ccad4a5fced1bf5",
+        INSTAGRAM_CLIENT_ID: "972db678348f44a3b511b5dcfe096acc",
         INSTAGRAM_REDIRECT_URI: "http://dev.nanapx.com:3000/login/callback",
         STRIPE_PUBLIC_KEY: "pk_test_rirrDsVemD2s5DOmg1P4HccZ"
       }
